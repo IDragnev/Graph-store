af917e957fe6a360f626eb3911cae210e5bb303b
IDragnev <iddragnev@gmail.com>
2018-05-09 22:15:40 +0300

    Add the selectBaseIterator template

---
+++ Graph store/Type selector/selectBaseIterator.h
@@ -0,0 +1,14 @@
+#ifndef __SELECT_ITERATOR_BASE_H_INCLUDED__
+#define __SELECT_ITERATOR_BASE_H_INCLUDED__
+
+#include "typeSelector.h"
+#include "../Iterator abstraction/ConstIterator.h"
+#include "../Iterator abstraction/Iterator.h"
+
+template <bool isConst, typename T>
+struct selectBaseIterator
+{
+	typedef typeSelector<isConst, ConstIterator<T>, Iterator<T>>::result result;
+};
+
+#endif //__SELECT_ITERATOR_BASE_H_INCLUDED__
