8507e8b94fdbd8174409bed6e8ea78e28a32c9c1
IDragnev <iddragnev@gmail.com>
2018-05-14 21:50:20 +0300

    Implement insertVertex and removeVertex in Graph

--- Graph store/Graph/Graph.cpp
+++ Graph store/Graph/Graph.cpp
@@ -21,3 +21,37 @@ Graph::~Graph()
 		++verticesIterator;
 	}
 }
+
+
+void Graph::insertVertexWithID(const char* ID)
+{
+	if (!existsVertexWithID(ID))
+	{
+		Vertex* newVertex = createVertex(ID);
+
+		insertToVertices(*newVertex);
+		insertToVerticesSearchTable(*newVertex);
+	}
+	else
+	{
+		//TODO: Graph exception class?
+		throw std::invalid_argument("A vertex with such ID already exists in " + this->id);
+	}
+}
+
+
+void Graph::removeVertexWithID(const char* ID)
+{
+	Vertex& vertexToRemove = getVertexWithID(ID);
+
+	removeFromAdjacencyLists(vertexToRemove);
+
+	removeFromVerticesSearchTable(vertexToRemove);
+	removeFromVertices(vertexToRemove);
+
+	deleteVertex(&vertexToRemove);
+}
+
+
+
+
