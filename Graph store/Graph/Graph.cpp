b2bc9d914ae8d11836b43874597ca3edc5d8d76e
IDragnev <iddragnev@gmail.com>
2018-05-14 22:50:59 +0300

    Implement the functions related to removing a vertex in Graph

--- Graph store/Graph/Graph.cpp
+++ Graph store/Graph/Graph.cpp
@@ -53,5 +53,68 @@ void Graph::removeVertexWithID(const char* ID)
 }
 
 
+//
+//the default implementation is for an unoriented graph:
+//each of the vertex' neighbours has an edge to it
+//
+void Graph::removeFromAdjacencyLists(Vertex& vertex)
+{
+	EdgeConstIterator adjacentEdgesIterator = vertex.edges.getHeadConstIterator();
 
+	while (adjacentEdgesIterator)
+	{
+		const Edge& currentEdge = *adjacentEdgesIterator;
+		Vertex* neighbour = currentEdge.incidentVertex;
+
+		removeEdgeFromToNoThrow(*neighbour, vertex);
+	}
+}
+
+
+void Graph::removeEdgeFromToNoThrow(Vertex& vertexFrom, Vertex& vertexTo)
+{
+	removeEdgeFromTo(vertexFrom, vertexTo, false);
+}
+
+
+void Graph::removeEdgeFromTo(Vertex& vertexFrom, Vertex& vertexTo)
+{
+	removeEdgeFromTo(vertexFrom, vertexTo, true);
+}
+
+
+void Graph::removeEdgeFromTo(Vertex& vertexFrom, Vertex& vertexTo, bool throwIfEdgeDoesNotExist)
+{
+	EdgeIterator iteratorToEdge = getEdgeFromTo(vertexFrom, vertexTo);
+
+	if (iteratorToEdge)
+	{
+		vertexFrom.edges.removeAt(iteratorToEdge);
+	}
+	else if (throwIfEdgeDoesNotExist)
+	{
+		//TODO: Graph exception class?
+		throw std::invalid_argument("There is no edge from " + vertexFrom.id + " to " + vertexTo.id);
+	}
+}
+
+
+Graph::EdgeIterator Graph::getEdgeFromTo(Vertex& vertexFrom, Vertex& vertexTo)
+{
+	EdgeIterator vertexFromEdgesIterator = vertexFrom.edges.getHeadIterator();
+
+	while (vertexFromEdgesIterator)
+	{
+		const Edge& currentEdge = *vertexFromEdgesIterator;
+
+		if (currentEdge.incidentVertex == &vertexTo)
+		{
+			break;
+		}
+
+		++vertexFromEdgesIterator;
+	}
+
+	return vertexFromEdgesIterator;
+}
 
