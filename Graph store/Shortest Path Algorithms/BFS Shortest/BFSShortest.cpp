149c0415f3c664e94c1e35fd3bf9950d46e84f98
IDragnev <iddragnev@gmail.com>
2018-05-30 22:08:39 +0300

    Implement the most high-level functions in BFSShortest

--- Graph store/Shortest Path Algorithms/BFS Shortest/BFSShortest.cpp
+++ Graph store/Shortest Path Algorithms/BFS Shortest/BFSShortest.cpp
@@ -15,3 +15,35 @@ BFSShortest::BFSShortest() :
 {
 	registerInstance();
 }
+
+
+void BFSShortest::findShortestPathInGraphFromTo(Graph& graph, Vertex& source, Vertex& goal)
+{
+	if (source != goal)
+	{
+		initializeState(graph, goal);
+		initializeSingleSource(graph, source);
+		findShortestPathFrom(source);
+		clearState();
+	}
+	else
+	{
+		buildTrivialPathFromTo(source, goal);
+	}
+}
+
+
+void BFSShortest::findShortestPathFrom(Vertex& source)
+{
+	assert(!isAShortestPathFound());
+	assert(isFrontierEmpty());
+
+	addToFrontier(source);
+
+	while (!(isAShortestPathFound() || isFrontierEmpty()))
+	{
+		Vertex& vertex = getVertexFromTheFrontier();
+
+		exploreNeighboursOf(vertex);
+	}
+}
