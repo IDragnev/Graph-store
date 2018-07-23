99174bf8ca4b27426ef97f6a368159c1dc7a5412
IDragnev <iddragnev@gmail.com>
2018-07-23 22:25:37 +0300

    Add the initial PriorityQueue class definition

---
+++ Graph store/Priority Queue/PriorityQueue.h
@@ -0,0 +1,43 @@
+#ifndef __PRIORITY_QUEUE_H_INCLUDED__
+#define __PRIORITY_QUEUE_H_INCLUDED__
+
+#include "Pair\Pair.h"
+#include "Priority Queue Handle\PriorityQueueHandle.h"
+#include "../Dynamic Array/DArray.h"
+
+#include "../Iterator abstraction/Iterator.h"
+
+template <typename Item, typename Key, typename CompareFunction, typename HandleSetter>
+class PriorityQueue
+{
+private:
+	typedef Pair<Item, Key> Element;
+	typedef DArray<Element> ElementArray;
+
+public:
+	PriorityQueue(size_t size);
+	PriorityQueue(Iterator<Element>& iterator, size_t size);
+	PriorityQueue(PriorityQueue&& source);
+	PriorityQueue(const PriorityQueue& source) = default;
+	~PriorityQueue() = default;
+
+	PriorityQueue& operator=(PriorityQueue&& rhs);
+	PriorityQueue& operator=(const PriorityQueue& rhs) = default;
+
+	bool isEmpty() const;
+
+	void insert(Element&& newElement);
+
+	Item& extractOptimal();
+	const Item& getOptimal() const;
+
+	void updateKey(PriorityQueueHandle handle, Key&& key);
+
+private:
+	ElementArray elements;
+	long lastElementIndex;
+	mutable CompareFunction compare;     
+	HandleSetter handleSetter;
+};
+
+#endif //__PRIORITY_QUEUE_H_INCLUDED__
