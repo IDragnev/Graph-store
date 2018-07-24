c5089bb8826854717f03eb3734c2b9e1365e1fcf
IDragnev <iddragnev@gmail.com>
2018-07-24 21:44:52 +0300

    Reorganize the PriorityQueue TEST_CLASS

--- Tests/Priority Queue Test/Priority Queue Test/PriorityQueueTest.cpp
+++ Tests/Priority Queue Test/Priority Queue Test/PriorityQueueTest.cpp
@@ -5,35 +5,73 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 #include "../../../Graph store/Priority Queue/PriorityQueue.h"
 
 #include "TestItem.h"
+#include "HandleSetter.h"
+#include "CompareFunction.h"
 
 namespace PriorityQueueTest
 {	
-	class CompareFunction
-	{
-	public:
-		bool operator()(int lhs, int rhs) { return lhs < rhs; }
-	};
-
-	class HandleSetter
-	{
-		void operator()(TestItem* itemPtr, const PriorityQueueHandle& handle) const
-		{
-			assert(itemPtr);
-			itemPtr->setHandle(handle);
-		}
-	};
-
 	TEST_CLASS(PriorityQueueTest)
 	{
 	private:
-		typedef PriorityQueue<TestItem, int, CompareFunction, HandleSetter> MaxPriorityQueue;
+		typedef PriorityQueue<TestItem, unsigned, CompareFunction, HandleSetter> MaxPriorityQueue;
+		typedef DArray<TestItem> TestItemArray;
+		typedef TestItemArray::Iterator ItemIterator;
+		typedef Pair<TestItem, unsigned> TestPair;
+
+		static TestItemArray testItems;
+		static const size_t TEST_ITEMS_COUNT = 8;
+
+	private:
+		static TestPair makePair(TestItem& item, unsigned key)
+		{
+			return TestPair(&item, key);
+		}
+
+		static void insertTestItemsByIDTo(MaxPriorityQueue& queue)
+		{
+			ItemIterator iterator = testItems.getHeadIterator();
+
+			while (iterator)
+			{
+				TestItem& currentItem = *iterator;
+				queue.insert(makePair(currentItem, currentItem.getID()));
+
+				++iterator;
+			}
+		}
+
+		static unsigned getGreatestItemID()
+		{
+			return testItems[TEST_ITEMS_COUNT - 1].getID();
+		}
 
 	public:	
+		TEST_CLASS_INITIALIZE(initializeItemsWithIncreasingIDs)
+		{
+			for (size_t i = 0; i < TEST_ITEMS_COUNT; ++i)
+			{
+				testItems[i].setID(i);
+			}
+		}
+
 		TEST_METHOD(testDefaultConstructedQueueIsEmpty)
 		{
 			MaxPriorityQueue queue;
 
 			Assert::IsTrue(queue.isEmpty());
 		}
+
+		TEST_METHOD(testInsertArrangesTheItemsCorrectly)
+		{
+			MaxPriorityQueue queue;
+			insertTestItemsByIDTo(queue);
+			
+			unsigned greatestID = getGreatestItemID();
+			const TestItem& optimal = queue.getOptimal();
+
+			Assert::AreEqual(optimal.getID(), greatestID);
+		}
 	};
+
+	PriorityQueueTest::TestItemArray PriorityQueueTest::testItems(TEST_ITEMS_COUNT, TEST_ITEMS_COUNT);
 }
