1f159aa2d451bd49d958cec18570c2f8f33fee6d
IDragnev <iddragnev@gmail.com>
2018-08-12 19:52:32 +0300

    Add the StringReader functor
    The third-party API requires it in order to read
    the String.

---
+++ Graph store/Application/Commands/StringReader/StringReader.h
@@ -0,0 +1,16 @@
+#ifndef __STRING_READER_H_INCLUDED__
+#define __STRING_READER_H_INCLUDED__
+
+#include <string>
+#include "..\..\..\String\String.h"
+
+class StringReader
+{
+public:
+	void operator()(const std::string& name, const std::string& value, String& destination) const;
+
+private:
+	static const size_t BUFFER_SIZE = 250;
+};
+
+#endif //__STRING_READER_H_INCLUDED__
