dcb725c0fe15a778eda33932120f92b6eea33850
IDragnev <iddragnev@gmail.com>
2018-08-12 19:54:09 +0300

    Add the UseGraphCommand class

---
+++ Graph store/Application/Commands/Use graph/UseGraphCommand.cpp
@@ -0,0 +1,48 @@
+#include "UseGraphCommand.h"
+
+
+void UseGraphCommand::execute(args::Subparser& parser)
+{
+	parseArguments(parser);
+	execute();
+}
+
+
+void UseGraphCommand::parseArguments(args::Subparser& parser)
+{
+	StringPositional id(parser, "ID", "the ID of the graph to be used");
+	parser.Parse();
+
+	setIdIfMatched(id);
+}
+
+
+void UseGraphCommand::setIdIfMatched(StringPositional& id)
+{
+	if (id)
+	{
+		graphID = args::get(id);
+	}
+	else
+	{
+		throw std::runtime_error("Missing argument: ID");
+	}
+}
+
+
+void UseGraphCommand::execute() const
+{
+	Command::useGraph(graphID);
+}
+
+
+const char* UseGraphCommand::getName() const
+{
+	return "USE-GRAPH";
+}
+
+
+const char* UseGraphCommand::getDescription() const
+{
+	return "Changes the used graph";
+}
---
+++ Graph store/Application/Commands/Use graph/UseGraphCommand.h
@@ -0,0 +1,35 @@
+#ifndef __USE_GRAPH_COMMAND_H_INCLUDED__
+#define __USE_GRAPH_COMMAND_H_INCLUDED__
+
+#include "..\Command.h"
+#include "..\StringReader\StringReader.h"
+
+
+class UseGraphCommand : public Command
+{
+private:
+	typedef args::Positional<String, StringReader> StringPositional;
+
+public:
+	UseGraphCommand() = default;
+	virtual ~UseGraphCommand() override = default;
+
+	virtual void execute(args::Subparser& parser) override;
+	virtual const char* getName() const override;
+	virtual const char* getDescription() const override;
+
+private:
+	UseGraphCommand(UseGraphCommand&&) = delete;
+	UseGraphCommand(const UseGraphCommand&) = delete;
+	UseGraphCommand& operator=(UseGraphCommand&&) = delete;
+	UseGraphCommand& operator=(const UseGraphCommand&) = delete;
+
+	void parseArguments(args::Subparser& parser);
+	void setIdIfMatched(StringPositional& id);
+	void execute() const;
+
+private:
+	String graphID;
+};
+
+#endif //__USE_GRAPH_COMMAND_H_INCLUDED__
