899df491a0c999ef2132ee90e3f232ea4bd0ca39
IDragnev <iddragnev@gmail.com>
2018-08-14 12:50:00 +0300

    Simplify the Exception class
    Inherited std::runtime_error's constructors.

--- Graph store/Exception/Exception.h
+++ Graph store/Exception/Exception.h
@@ -7,10 +7,8 @@ template <typename T>
 class Exception : public std::runtime_error
 {
 public:
-	Exception(const char* message);
-	Exception(const std::string& message);
+	using std::runtime_error::runtime_error;
 	virtual ~Exception() override = default;
 };
 
-#include "ExceptionImpl.hpp"
 #endif //__MY_EXCEPTION_H_INCLUDED__
