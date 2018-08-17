476c503ff3a927854bfc4e6f699c3711576502f5
IDragnev <iddragnev@gmail.com>
2018-08-17 22:15:16 +0300

    Make massive refactoring of SinglyLinkedList unit tests

--- Tests/Singly Linked list test/SinglyLinkedListTest.cpp
+++ Tests/Singly Linked list test/SinglyLinkedListTest.cpp
@@ -1,5 +1,6 @@
 #include "CppUnitTest.h"
 #include "../../Graph store/Singly Linked List/SinglyLinkedList.h"
+#include <algorithm>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,64 +13,6 @@ namespace SinglyLinkedListTest
 		typedef SinglyLinkedList<int>::ConstIterator ListConstIterator;
 		typedef SinglyLinkedList<int> List;
 
-		static const int BIG_INTEGER = 100;
-
-		static void fillListWithIntegersFromZeroTo(List& list, const int integer)
-		{
-			for (int i = 0; i <= integer; ++i)
-			{
-				list.insertAsTail(i);
-			}
-		}
-
-		static void fillListWithIntegersFromZeroToInReverse(List& list, const int integer)
-		{
-			for (int i = 0; i <= integer; ++i)
-			{
-				list.insertAsHead(i);
-			}
-		}
-
-		static bool iterateSameContents(ListConstIterator& lhs, ListConstIterator& rhs)
-		{
-			while (lhs)
-			{
-				if (*lhs != *rhs)
-				{
-					return false;
-				}
-
-				++lhs;
-				++rhs;
-			}
-
-			return !lhs && !rhs;
-		}
-
-		static bool areEqual(const List& lhs, const List& rhs)
-		{
-			return (lhs.getCount() == rhs.getCount()) && iterateSameContents(lhs.getHeadConstIterator(), rhs.getHeadConstIterator());
-		}
-
-		static bool containsAllIntegersFromZeroTo(const List& list, const int integer)
-		{
-			ListConstIterator constIterator = list.getHeadConstIterator();
-			int currentInteger = 0;
-
-			while (constIterator)
-			{
-				if (*constIterator != currentInteger)
-				{
-					return false;
-				}
-
-				++constIterator;
-				++currentInteger;
-			}
-
-			return currentInteger - 1 == integer;
-		}
-
 	public:
 		TEST_METHOD(testDefaultConstructedListIsEmpty)
 		{
@@ -78,316 +21,205 @@ namespace SinglyLinkedListTest
 			Assert::IsTrue(list.isEmpty());
 		}
 
+		TEST_METHOD(testInitializerListCtor)
+		{
+			List list{ 0, 1, 2, 3, 4 };
+
+			int i = 0;
+			std::for_each(list.getBeginConstIterator(), list.getEndConstIterator(), 
+				[&](int current)
+			{
+				Assert::AreEqual(current, i++);
+			});		
+		}
+
 		TEST_METHOD(testEmptyListReturnsNullIterators)
 		{
 			List list;
 
-			ListConstIterator head = list.getHeadConstIterator();
-			ListConstIterator tail = list.getTailConstIterator();
+			ListConstIterator head = list.getBeginConstIterator();
+			ListConstIterator tail = list.getEndConstIterator();
 
-			Assert::IsFalse(head, L"Head iterator of empty list is not null");
-			Assert::IsFalse(tail, L"Tail iterator of empty list is not null");
+			Assert::IsFalse(head, L"Begin iterator of empty list is not null");
+			Assert::IsFalse(tail, L"End iterator of empty list is not null");
 		}
 	
 		TEST_METHOD(testTailInsertionUpdatesCountAndTail)
 		{
 			List list;
 
-			for (int i = 0; i < BIG_INTEGER; ++i)
-			{
-				list.insertAsTail(i);
+			list.insertAsTail(0);
 
-				Assert::AreEqual(list.getCount(), i + 1U, L"Count is not updated when inserting as tail");
-				Assert::AreEqual(list.getTail(), i, L"Tail is not updated when inserting as tail");
-			}
+			Assert::AreEqual(list.getCount(), 1U, L"Count is not updated");
+			Assert::AreEqual(list.getTail(), 0, L"Tail is not updated");
 		}
 
 		TEST_METHOD(testInsertionAsHeadUpdatesCountAndHead)
 		{
 			List list;
 
-			for (int i = 0; i < BIG_INTEGER; ++i)
-			{
-				list.insertAsHead(i);
+			list.insertAsHead(0);
 
-				Assert::AreEqual(list.getCount(), i + 1U, L"Count is not updated when inserting as head");
-				Assert::AreEqual(list.getHead(), i, L"Head is not updated when inserting as head");
-			}
+			Assert::AreEqual(list.getCount(), 1U, L"Count is not updated");
+			Assert::AreEqual(list.getHead(), 0, L"Head is not updated");
 		}
 		
-		TEST_METHOD(testAppendList)
+		TEST_METHOD(testAppendListLValue)
 		{
-			List source;
-			List destination;
-			fillListWithIntegersFromZeroTo(source, BIG_INTEGER);
-			fillListWithIntegersFromZeroTo(destination, BIG_INTEGER / 2);
-
-			ListConstIterator destinationIterator = destination.getTailConstIterator();
-			const int destinationInitialCount = destination.getCount();
-			ListConstIterator sourceIterator = source.getHeadConstIterator();
-
+			List source{ 4, 5, 6 };
+			List destination{ 1, 2, 3 };
+			
 			destination.appendList(source);
-			++destinationIterator;
 
-			Assert::IsTrue(destinationIterator, L"There is no item after tail after appending non-empty list");
-			Assert::AreEqual(destination.getCount(), destinationInitialCount + source.getCount(), L"Appending list does not update count");
-			Assert::IsTrue(iterateSameContents(destinationIterator, sourceIterator), L"Appended list is not the same in destination");
+			Assert::IsTrue(destination == List{ 1, 2, 3, 4, 5, 6 });
+		}
+
+		TEST_METHOD(testAppendListRValue)
+		{
+			List source{ 4, 5, 6 };
+			List destination{ 1, 2, 3 };
+
+			destination.appendList(std::move(source));
+
+			Assert::IsTrue(destination == List{ 1, 2, 3, 4, 5, 6 }, L"Appended-to list has invalid contents");
+			Assert::IsTrue(source.isEmpty(), L"Move-appended list is not empty");
 		}
 
 		TEST_METHOD(testRemoveHeadUpdatesCountAndHead)
 		{
-			List list;
-			fillListWithIntegersFromZeroToInReverse(list, BIG_INTEGER - 1);
+			List list{ 1, 2, 3 };
+			
+			list.removeHead();
 
-			for (int i = BIG_INTEGER; i > 0; --i)
-			{
-				Assert::AreEqual(list.getHead(), i - 1, L"Removing head does not update head");
-				Assert::IsTrue(list.getCount() == i, L"Removing head does not update count");
-
-				list.removeHead();
-			}
+			Assert::AreEqual(list.getHead(), 2, L"Head is not updated");
+			Assert::AreEqual(list.getCount(), 2U, L"Count is not updated");
 		}
 		
 		TEST_METHOD(testRemoveTailUpdatesCoundAndTail)
 		{
-			List list;
-			fillListWithIntegersFromZeroTo(list, BIG_INTEGER - 1);
+			List list{ 1, 2, 3 };
 
-			for (int i = BIG_INTEGER; i > 0; --i)
-			{
-				Assert::AreEqual(list.getTail(), i - 1, L"Removing tail does not update tail");
-				Assert::IsTrue(list.getCount() == i, L"Removing tail does not update count");
+			list.removeTail();
 
-				list.removeTail();
-			}
+			Assert::AreEqual(list.getTail(), 2, L"Tail is not updated");
+			Assert::AreEqual(list.getCount(), 2U, L"Count is not updated");
 		}
 
 		TEST_METHOD(testInsertionAfterNullIteratorInsertsAsTail)
 		{
-			List list;
+			List list{ 10, 11, 12 };
+			ListIterator iterator = list.getEndIterator();
 
-			for (int i = 0; i < BIG_INTEGER; ++i)
-			{
-				ListIterator iterator = list.getTailIterator();
-				//forse null
-				++iterator;
+			list.insertAfter(iterator, 1);
 
-				list.insertAfter(iterator, i);
-
-				Assert::AreEqual(list.getTail(), i);
-			}
+			Assert::AreEqual(list.getTail(), 1);
 		}
 
-		TEST_METHOD(testInsertionAfterHeadIterator)
+		TEST_METHOD(testInsertionAfterValidIterator)
 		{
-			List list;
-			list.insertAsHead(1);
+			List list{ 1 };
+			ListIterator iterator = list.getBeginIterator();
 
-			for (int i = 0; i < BIG_INTEGER; ++i)
-			{
-				ListIterator headIterator = list.getHeadIterator();
+			list.insertAfter(iterator, 2);
 
-				list.insertAfter(headIterator, i);
-
-				++headIterator;
-
-				Assert::IsTrue(headIterator, L"There is no object after the object we inserted after");
-				Assert::AreEqual(*headIterator, i, L"The inserted object is not the one passed as argument");
-			}
+			Assert::AreEqual(list.getTail(), 2);
 		}
 
 		TEST_METHOD(testInsertionBetweenElementsWithInsertAfterIterator)
 		{
-			List list;
-			list.insertAsHead(1);
-			list.insertAsTail(3);
+			List list{ 1, 3 };
+			
+			ListIterator iterator = list.getBeginIterator();
+			list.insertAfter(iterator, 2);
 
-			ListIterator headIterator = list.getHeadIterator();
-			list.insertAfter(headIterator, 2);
-
-			int i = 1;
-			while(headIterator)
-			{
-				Assert::AreEqual(*headIterator, i);
-
-				++headIterator;
-				++i;
-			}
-		}
-
-		TEST_METHOD(testInsertionAfterTailIterator)
-		{
-			List list;
-			list.insertAsTail(1);
-
-			for (int i = 0; i < BIG_INTEGER; ++i)
-			{
-				ListIterator tailIterator = list.getTailIterator();
-
-				list.insertAfter(tailIterator, i);
-
-				++tailIterator;
-
-				Assert::IsTrue(tailIterator, L"There is no object after the object we inserted after");
-				Assert::AreEqual(*tailIterator, i, L"The inserted object is not the one passed as argument");
-			}
+			Assert::IsTrue(list == List{ 1, 2, 3 });
 		}
 
 		TEST_METHOD(testInsertionBeforeNullIteratorInsertsAsHead)
 		{
-			List list;
+			List list{ 10 };
 
-			for (int i = 0; i < BIG_INTEGER; ++i)
-			{
-				ListIterator iterator = list.getTailIterator();
-				//forse null
-				++iterator;
+			ListIterator iterator = list.getEndIterator();
+			list.insertBefore(iterator, 1);
 
-				list.insertBefore(iterator, i);
-
-				Assert::AreEqual(list.getHead(), i);
-			}
+			Assert::AreEqual(list.getHead(), 1);
 		}
 
-		TEST_METHOD(testInsertionBeforeHeadIterator)
+		TEST_METHOD(testInsertionBeforeIterator)
 		{
-			List list;
-			list.insertAsHead(1);
+			List list{ 1 };
 
-			for (int i = 0; i < BIG_INTEGER; ++i)
-			{
-				ListIterator headIterator = list.getHeadIterator();
+			ListIterator iterator = list.getBeginIterator();
+			list.insertBefore(iterator, 20);
 
-				list.insertBefore(headIterator, i);
-
-				Assert::AreEqual(list.getHead(), i);
-			}
+			Assert::AreEqual(list.getHead(), 20);
 		}
 
 		TEST_METHOD(testInsertionBetweenElementsWithInsertBeforeIterator)
 		{
-			List list;
-			list.insertAsHead(1);
-			list.insertAsTail(3);
-
-			ListIterator iterator = list.getTailIterator();
+			List list{ 1,3 };
+			ListIterator iterator = list.getBeginIterator();
+			++iterator;
 
 			list.insertBefore(iterator, 2);
 
-			iterator = list.getHeadIterator();
-			int i = 1;
-			while(iterator)
-			{
-				Assert::AreEqual(*iterator, i);
-
-				++iterator;
-				++i;
-			}
+			Assert::IsTrue(list == List{ 1, 2, 3 });
 		}
 
 		TEST_METHOD(testRemovalAtNullIteratorDoesNothing)
 		{
 			List emptyList;
-			ListIterator nullIterator = emptyList.getHeadIterator();
+			ListIterator nullIterator = emptyList.getEndIterator();
 
 			emptyList.removeAt(nullIterator);
 
 			Assert::IsTrue(emptyList.isEmpty());
 		}
 
-		TEST_METHOD(testRemovalAtHeadIterator)
+		TEST_METHOD(testRemovalAtIterator)
 		{
-			List list;
-			fillListWithIntegersFromZeroTo(list, BIG_INTEGER - 1);
-
-			for (int i = 0; i < BIG_INTEGER; ++i)
-			{
-				ListIterator headIterator = list.getHeadIterator();
-				list.removeAt(headIterator);
-
-				Assert::IsFalse(headIterator, L"Removing at head iterator does not invalidate the iterator");
-
-				if (!list.isEmpty())
-				{
-					Assert::AreEqual(list.getHead(), i + 1, L"Removing at head iterator does not update head");
-				}
-
-				Assert::IsTrue(list.getCount() == BIG_INTEGER - (i + 1), L"Removing at head iterator does not update count");
-			}
-		}
-
-		TEST_METHOD(testRemovalAtTailIterator)
-		{
-			List list;
-			fillListWithIntegersFromZeroTo(list, BIG_INTEGER - 1);
-
-			for (int i = BIG_INTEGER - 1; i >= 0; --i)
-			{
-				ListIterator tailIterator = list.getTailIterator();
-				list.removeAt(tailIterator);
-
-				Assert::IsFalse(tailIterator, L"Removing at iterator does not invalidate the iterator");
-
-				if (!list.isEmpty())
-				{
-					Assert::AreEqual(list.getTail(), i - 1, L"Removing at tail iterator does not update tail");
-				}
-
-				Assert::IsTrue(list.getCount() == i, L"Removing at tail iterator does not update count");
-			}
-		}
-
-		TEST_METHOD(testRemovalBetweenElementsWithIterator)
-		{
-			List list;
-			fillListWithIntegersFromZeroTo(list, 2);
-
-			ListIterator iterator = list.getHeadIterator();
+			List list{ 1, 2, 3 };
+			ListIterator iterator = list.getBeginIterator();
 			++iterator;
 
 			list.removeAt(iterator);
 
-			Assert::IsFalse(iterator, L"Iterator is not invalidated after removing at it");
-			Assert::AreEqual(list.getCount(), 2U, L"Removing at iterator does not update count");
-			Assert::AreEqual(list.getTail(), 2, L"Removing between elements does not update successor");
+			Assert::IsTrue(list == List{ 1, 3 });
+
 		}
 
 		TEST_METHOD(testCopyCtorFromEmptySource)
 		{
 			List source;
-			List destination(source);
+			List destination{ source };
 
-			Assert::IsTrue(areEqual(source, destination));
+			Assert::IsTrue(source == destination);
 		}
 
 		TEST_METHOD(testCopyCtorFromNonEmptySource)
 		{
-			List source;
-			fillListWithIntegersFromZeroTo(source, BIG_INTEGER);
+			List source{ 1, 2, 3 };
+			List destination{ source };
 
-			List destination(source);
-
-			Assert::IsTrue(areEqual(source, destination));
+			Assert::IsTrue(source == destination);
 		}
 
 		TEST_METHOD(testMoveCtorFromEmptySource)
 		{
 			List source;
-			List destination(std::move(source));
+			List destination{ std::move(source) };
 
-			Assert::IsTrue(destination.isEmpty(), L"Destination is not empty after moving an empty source in it");
-			Assert::IsTrue(source.isEmpty(), L"Source is not empty after moving it");
+			Assert::IsTrue(destination.isEmpty(), L"Moved-in list is not empty");
+			Assert::IsTrue(source.isEmpty(), L"Moved-from list is not empty");
 		}
 
 		TEST_METHOD(testMoveCtorFromNonEmptySource)
 		{
-			List source;
-			fillListWithIntegersFromZeroTo(source, BIG_INTEGER);
+			List source{ 1, 2, 3, 4 };
+			List destination{ std::move(source) };
 
-			List destination(std::move(source));
-
-			Assert::IsTrue(containsAllIntegersFromZeroTo(destination, BIG_INTEGER), L"Destination has different contents from the moved-in source");
-			Assert::IsTrue(source.isEmpty(), L"Source is not empty after moving it");
+			Assert::IsTrue(destination == List{1, 2, 3, 4}, L"Moved-in list has invalid contents");
+			Assert::IsTrue(source.isEmpty(), L"Moved-from list is not empty");
 		}
 
 		TEST_METHOD(testCopyAssignmentEmptyToEmpty)
@@ -397,41 +229,37 @@ namespace SinglyLinkedListTest
 
 			lhs = rhs;
 
-			Assert::IsTrue(areEqual(lhs, rhs));
+			Assert::IsTrue(lhs == rhs);
 		}
 
 		TEST_METHOD(testCopyAssignmentNonEmptyToEmpty)
 		{
 			List lhs;
-			List rhs;
-			fillListWithIntegersFromZeroTo(rhs, BIG_INTEGER);
+			List rhs{ 1, 2, 3, 4 };
 
 			lhs = rhs;
 
-			Assert::IsTrue(areEqual(lhs, rhs));
+			Assert::IsTrue(lhs == rhs);
 		}
 
 		TEST_METHOD(testCopyAssignmentNonEmptyToNonEmpty)
 		{
-			List lhs;
-			List rhs;
-			fillListWithIntegersFromZeroToInReverse(lhs, BIG_INTEGER);
-			fillListWithIntegersFromZeroTo(rhs, BIG_INTEGER / 2);
+			List lhs{ 1, 2, 3, 4 };
+			List rhs{ 12, 13, 14, 15 };
 
 			lhs = rhs;
 
-			Assert::IsTrue(areEqual(lhs, rhs));
+			Assert::IsTrue(lhs ==  rhs);
 		}
 
 		TEST_METHOD(testCopyAssignmentEmptyToNonEmpty)
 		{
-			List lhs;
+			List lhs{ 1, 2, 3, 4 };
 			List rhs;
-			fillListWithIntegersFromZeroTo(lhs, BIG_INTEGER);
 
 			lhs = rhs;
 
-			Assert::IsTrue(areEqual(lhs, rhs));
+			Assert::IsTrue(lhs == rhs);
 		}
 		
 		TEST_METHOD(testMoveAssignmentEmptyToEmpty)
@@ -441,62 +269,41 @@ namespace SinglyLinkedListTest
 
 			lhs = std::move(rhs);
 
-			Assert::IsTrue(lhs.isEmpty(), L"Lhs is not empty after move-assigning it an empty rhs");
-			Assert::IsTrue(rhs.isEmpty(), L"Rhs is not empty after move-assigning it");
+			Assert::IsTrue(lhs.isEmpty(), L"Moved-in list is not empty");
+			Assert::IsTrue(rhs.isEmpty(), L"Moved-from list is not empty");
 		}
 
 		TEST_METHOD(testMoveAssignmentNonEmptyToEmpty)
 		{
 			List lhs;
-			List rhs;
-			fillListWithIntegersFromZeroTo(rhs, BIG_INTEGER);
+			List rhs{ 1, 2, 3, 4 };
 
 			lhs = std::move(rhs);
 
-			Assert::IsTrue(containsAllIntegersFromZeroTo(lhs, BIG_INTEGER), L"Lhs has different contents from the move-assignmed-from Rhs");
-			Assert::IsTrue(rhs.isEmpty(), L"Rhs is not empty after move-assigning it");
+			Assert::IsTrue(lhs == List{ 1, 2, 3, 4 }, L"Moved-in list has invalid contents");
+			Assert::IsTrue(rhs.isEmpty(), L"Moved-from list is not empty");
 		}
 		
 		TEST_METHOD(testMoveAssignmentEmptyToNonEmpty)
 		{
-			List lhs;
+			List lhs{ 1, 2, 3, 4 };
 			List rhs;
-			fillListWithIntegersFromZeroTo(lhs, BIG_INTEGER);
 
 			lhs = std::move(rhs);
 
-			Assert::IsTrue(lhs.isEmpty(), L"Lhs is not empty after move-assigning it an empty rhs");
-			Assert::IsTrue(rhs.isEmpty(), L"Rhs is not empty after move-assigning it");
+			Assert::IsTrue(lhs.isEmpty(), L"Moved-in list is not empty");
+			Assert::IsTrue(rhs.isEmpty(), L"Moved-from list is not empty");
 		}
 
 		TEST_METHOD(testMoveAssignmentNonEmptyToNonEmpty)
 		{
-			List lhs;
-			List rhs;
-			fillListWithIntegersFromZeroToInReverse(lhs, BIG_INTEGER - 5);
-			fillListWithIntegersFromZeroTo(rhs, BIG_INTEGER);
+			List lhs{ 1, 2, 3, 4 };
+			List rhs{ 10, 9, 8, 7 };
 
 			lhs = std::move(rhs);
 
-			Assert::IsTrue(containsAllIntegersFromZeroTo(lhs, BIG_INTEGER), L"Lhs has different contents from the move-assignmed-from Rhs");
-			Assert::IsTrue(rhs.isEmpty(), L"Rhs is not empty after move-assigning it");
-		}
-
-		TEST_METHOD(testIterator)
-		{
-			List list;
-			fillListWithIntegersFromZeroTo(list, BIG_INTEGER - 1);
-
-			ListConstIterator iterator = list.getHeadConstIterator();
-			int i = 0;
-
-			for (; iterator; ++iterator)
-			{
-				Assert::AreEqual(*iterator, i);
-				++i;
-			}
-
-			Assert::AreEqual(i, BIG_INTEGER);
+			Assert::IsTrue(lhs == List{10, 9, 8, 7}, L"Moved-in list has invalid contents");
+			Assert::IsTrue(rhs.isEmpty(), L"Moved-from list is not empty");
 		}
 	};
 }
