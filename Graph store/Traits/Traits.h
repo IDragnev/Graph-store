4dafa1e614d4622c5755c730ef3195abf3bcb350
IDragnev <iddragnev@gmail.com>
2018-09-01 22:15:25 +0300

    Add iterator trait classes

--- Graph store/Traits/Traits.h
+++ Graph store/Traits/Traits.h
@@ -19,4 +19,34 @@ struct is_const_iterator
 	static constexpr bool value = typename is_const_reference<reference>::value;
 };
 
+
+template <typename Iterator>
+struct is_random_access_iterator
+{
+	using category = typename std::iterator_traits<Iterator>::iterator_category;
+	static constexpr bool value = typename std::is_same<category, std::random_access_iterator_tag>::value;
+};
+
+
+template <typename Iterator>
+struct is_bidirectional_iterator
+{
+	using category = typename std::iterator_traits<Iterator>::iterator_category;
+	static constexpr bool isRandomAccess = typename is_random_access_iterator<Iterator>::value;
+	static constexpr bool isBidirectional = typename std::is_same<category, std::bidirectional_iterator_tag>::value;
+
+	static constexpr bool value = isRandomAccess || isBidirectional;
+};
+
+
+template <typename Iterator>
+struct is_forward_iterator
+{
+	using category = typename std::iterator_traits<Iterator>::iterator_category;
+	static constexpr bool isRandomAccess = typename is_random_access_iterator<Iterator>::value;
+	static constexpr bool isForward = typename std::is_same<category, std::forward_iterator_tag>::value;
+
+	static constexpr bool value = isForward || isRandomAccess;
+};
+
 #endif //__TRAITS_H_INCLUDED__
