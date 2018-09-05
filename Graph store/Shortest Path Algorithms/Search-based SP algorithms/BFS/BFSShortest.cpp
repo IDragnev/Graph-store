f79bda18947bb51f0a315dcc228a6261376d4bc6
IDragnev <iddragnev@gmail.com>
2018-09-05 23:03:26 +0300

    Refactor BFSShortest

---
+++ Graph store/Shortest Path Algorithms/Search-based SP algorithms/BFS/BFSShortest.cpp
@@ -0,0 +1,100 @@
+#include "BFSShortest.h"
+#include "..\..\..\ShortestPathAlgorithm Store\Algorithm registrator\ShortestPathAlgorithmRegistrator.h"
+
+static ShortestPathAlgorithmRegistrator<BFSShortest> registrator{ "BFS" };
+
+
+ShortestPathAlgorithm::Path 
+BFSShortest::findNonTrivialShortestPath(const Graph& graph, const Vertex& source, const Vertex& goal)
+{
+	decorate(graph, source);
+	auto result = findShortestPath(source, goal);
+	clean();
+
+	return result;
+}
+
+
+ShortestPathAlgorithm::Path
+BFSShortest::findShortestPath(const Vertex& source, const Vertex& goal)
+{
+	assert(isFrontierEmpty());
+	addToFrontier(decoratorOf(source));
+
+	while (!isFrontierEmpty())
+	{
+		auto& vertex = extractVertexFromFrontier();
+
+		if (isTheGoal(vertex))
+		{
+			break;
+		}
+		else
+		{
+			expandFrontierFrom(vertex);
+		}
+	}
+
+	return Path{ decoratorOf(goal) };
+}
+
+
+bool BFSShortest::isFrontierEmpty() const
+{
+	return queue.isEmpty();
+}
+
+
+void BFSShortest::addToFrontier(const MarkableVertex& v)
+{
+	assert(v.isVisited);
+	queue.enqueue(&v);
+}
+
+
+const BFSShortest::MarkableVertex&
+BFSShortest::extractVertexFromFrontier()
+{
+	auto* vertex = queue.dequeue();
+	assert(vertex);
+
+	return *vertex;
+}
+
+
+void BFSShortest::expandFrontierFrom(const MarkableVertex& vertex)
+{
+	auto iteratorPtr = getEdgesLeaving(vertex);
+
+	forEach(*iteratorPtr, [&](const Edge& edge) 
+	{
+		auto& neighbour = decoratorOf(edge.getIncidentVertex());
+
+		if (!neighbour.isVisited)
+		{
+			neighbour.isVisited = true;
+			extendCurrentPathFromTo(vertex, neighbour);
+			addToFrontier(neighbour);
+		}
+	});
+}
+
+
+void BFSShortest::extendCurrentPathFromTo(const MarkableVertex& from, MarkableVertex& to)
+{
+	to.predecessor = &from;
+	to.distance = from.distance + Distance{ 1 };
+}
+
+
+void BFSShortest::clean()
+{
+	queue.empty();
+	cleanDecoratedState();
+}
+
+
+void BFSShortest::initSourceDecorator(MarkableVertex& source)
+{
+	Base::initSourceDecorator(source);
+}
---
+++ Graph store/Shortest Path Algorithms/Search-based SP algorithms/BFS/BFSShortest.h
@@ -0,0 +1,35 @@
+#ifndef __BFS_SHORTEST_PATH_H_INCLUDED__
+#define __BFS_SHORTEST_PATH_H_INCLUDED__
+
+#include "..\Base\SearchBasedShortestPathAlgorithm.h"
+#include "..\..\..\Queue\Queue.h"
+
+class BFSShortest: public SearchBasedShortestPathAlgorithm
+{
+private:
+	using VertexDecoratorsQueue = Queue<const MarkableVertex*>;
+	using Base = SearchBasedShortestPathAlgorithm;
+
+public:
+	using SearchBasedShortestPathAlgorithm::SearchBasedShortestPathAlgorithm;
+
+private:
+	virtual Path findNonTrivialShortestPath(const Graph& graph, 
+											const Vertex& source, 
+											const Vertex& goal) override;
+
+	Path findShortestPath(const Vertex& source, const Vertex& goal);
+	bool isFrontierEmpty() const;
+	const MarkableVertex& extractVertexFromFrontier();
+	void expandFrontierFrom(const MarkableVertex& v);
+	void addToFrontier(const MarkableVertex& v);
+	static void extendCurrentPathFromTo(const MarkableVertex& from, MarkableVertex& to);
+	void clean();
+
+	virtual void initSourceDecorator(MarkableVertex& source) override;
+
+private:
+	VertexDecoratorsQueue queue{};
+};
+
+#endif //__BFS_SHORTEST_PATH_H_INCLUDED__
