8c8008bf50ad6af1c4bdaeed1aade4ab7bf6b766
IDragnev <iddragnev@gmail.com>
2018-10-18 23:18:09 +0300

    Nest the trait classes in a Traits namespace

--- Graph store/Traits/Traits.h
+++ Graph store/Traits/Traits.h
@@ -4,36 +4,40 @@
 #include <type_traits>
 #include <iterator>
 
-template <typename T>
-struct IsConstReference
+namespace IDragnev
 {
-private:
-	using RawType = std::remove_reference_t<T>;
+	namespace Traits
+	{
+		template <typename T>
+		struct IsConstReference
+		{
+		private:
+			using RawType = std::remove_reference_t<T>;
 
-public:
-	static constexpr bool value = std::is_const<RawType>::value;
-};
+		public:
+			static constexpr bool value = std::is_const<RawType>::value;
+		};
 
+		template <typename Iterator>
+		struct IsConstIterator
+		{
+		private:
+			using reference = typename std::iterator_traits<Iterator>::reference;
 
-template <typename Iterator>
-struct IsConstIterator
-{
-private:
-	using reference = typename std::iterator_traits<Iterator>::reference;
+		public:
+			static constexpr bool value = IsConstReference<reference>::value;
+		};
 
-public:
-	static constexpr bool value = IsConstReference<reference>::value;
-};
+		template <typename Iterator>
+		struct IsForwardIterator
+		{
+		private:
+			using iterator_category = typename std::iterator_traits<Iterator>::iterator_category;
 
-
-template <typename Iterator>
-struct IsForwardIterator
-{
-private:
-	using iterator_category = typename std::iterator_traits<Iterator>::iterator_category;
-
-public:
-	static constexpr bool value = std::is_convertible<iterator_category, std::forward_iterator_tag>::value;
-};
+		public:
+			static constexpr bool value = std::is_convertible<iterator_category, std::forward_iterator_tag>::value;
+		};
+	}
+}
 
 #endif //__TRAITS_H_INCLUDED__
