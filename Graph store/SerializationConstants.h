0657b39ba73d42ac583fac550071f19806ef8c37
IDragnev <iddragnev@gmail.com>
2019-01-19 22:11:51 +0200

    Move common constants in a separate header
    Moved the serialization constants which are common
    in GraphSaver and GraphBuilder to a detail namespace in
    a separate header file.

---
+++ Graph store/SerializationConstants.h
@@ -0,0 +1,17 @@
+#ifndef __SERIZALIZATION_CONSTANTS_H_INCLUDED__
+#define __SERIZALIZATION_CONSTANTS_H_INCLUDED__
+
+namespace IDragnev
+{
+	namespace GraphStore
+	{
+		namespace SerializationConstants
+		{
+			static const char EDGE_START = '(';
+			static const char EDGE_END = ')';
+			static const char EDGE_ATTRIBUTE_DELIMITER = ',';
+		}
+	}
+}
+
+#endif //__SERIZALIZATION_CONSTANTS_H_INCLUDED__
