fd268e3b1e7f3e7f0196a1e84fdc628cdd4ca5a7
IDragnev <iddragnev@gmail.com>
2019-02-15 13:31:08 +0200

    Refactor the trait classes
    Nested them all in a detail namespace and wrote inlined constants
    for easier use.

--- Graph store/Traits/Traits.h
+++ Graph store/Traits/Traits.h
@@ -8,35 +8,47 @@ namespace IDragnev
 {
 	namespace Traits
 	{
+		namespace Detail
+		{
+			template <typename T>
+			struct IsConstReferenceImpl
+			{
+			private:
+				using RawType = std::remove_reference_t<T>;
+
+			public:
+				static constexpr bool value = std::is_const<RawType>::value;
+			};
+
+			template <typename Iterator>
+			struct IsConstIteratorImpl
+			{
+			private:
+				using reference = typename std::iterator_traits<Iterator>::reference;
+
+			public:
+				static constexpr bool value = IsConstReferenceImpl<reference>::value;
+			};
+
+			template <typename Iterator>
+			struct IsForwardIteratorImpl
+			{
+			private:
+				using iterator_category = typename std::iterator_traits<Iterator>::iterator_category;
+
+			public:
+				static constexpr bool value = std::is_convertible<iterator_category, std::forward_iterator_tag>::value;
+			};
+		}
+
 		template <typename T>
-		struct IsConstReference
-		{
-		private:
-			using RawType = std::remove_reference_t<T>;
+		inline constexpr bool IsConstReference = Detail::IsConstReferenceImpl<T>::value;
 
-		public:
-			static constexpr bool value = std::is_const<RawType>::value;
-		};
+		template <typename T>
+		inline constexpr bool IsConstIterator = Detail::IsConstIteratorImpl<T>::value;
 
-		template <typename Iterator>
-		struct IsConstIterator
-		{
-		private:
-			using reference = typename std::iterator_traits<Iterator>::reference;
-
-		public:
-			static constexpr bool value = IsConstReference<reference>::value;
-		};
-
-		template <typename Iterator>
-		struct IsForwardIterator
-		{
-		private:
-			using iterator_category = typename std::iterator_traits<Iterator>::iterator_category;
-
-		public:
-			static constexpr bool value = std::is_convertible<iterator_category, std::forward_iterator_tag>::value;
-		};
+		template <typename T>
+		inline constexpr bool IsForwardIterator = Detail::IsForwardIteratorImpl<T>::value;
 	}
 }
 
