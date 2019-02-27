ce73cc40bf3fd949ff3cb3c8f91521bda3819544
IDragnev <iddragnev@gmail.com>
2019-02-27 21:36:56 +0200

    Add the GraphUtilities header

---
+++ Graph store/Graph/Base Graph/GraphUtilities.h
@@ -0,0 +1,44 @@
+#ifndef __GRAPH_UTILS_H_INCLUDED__
+#define __GRAPH_UTILS_H_INCLUDED__
+
+#include "Graph.h"
+#include <type_traits>
+
+namespace IDragnev
+{
+	namespace GraphStore
+	{
+		namespace Detail
+		{
+			template <typename Callable>
+			inline static constexpr bool readsVertex = std::is_invocable_v<Callable, const Graph::Vertex>;
+
+			template <typename Callable>
+			inline static constexpr bool modifiesLvalueVertex = !readsVertex<Callable> && std::is_invocable_v<Callable, Graph::Vertex&>;
+
+			template <typename Callable>
+			using ReadsVertex = std::enable_if_t<readsVertex<Callable>>;
+
+			template <typename Callable>
+			using ModifiesLvalueVertex = std::enable_if_t<modifiesLvalueVertex<Callable>>;
+		}
+
+		template <typename Callable,
+			      typename = Detail::ReadsVertex<Callable>>
+		inline void forEachVertex(const Graph& g, Callable f) 
+		{
+			auto iteratorPtr = g.getConstIteratorToVertices();
+			forEach(*iteratorPtr, f);
+		}
+
+		template <typename Callable,
+			      typename = Detail::ModifiesLvalueVertex<Callable>>
+		inline void forEachVertex(Graph& g, Callable f)
+		{
+			auto iteratorPtr = g.getIteratorToVertices();
+			forEach(*iteratorPtr, f);
+		}
+	}
+}
+
+#endif //__GRAPH_UTILS_H_INCLUDED__
