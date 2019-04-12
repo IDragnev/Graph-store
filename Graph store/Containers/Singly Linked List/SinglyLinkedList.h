e19a7a810b7c67e165ed83341f4179d190036140
IDragnev <iddragnev@gmail.com>
2019-04-12 22:41:52 +0300

    Start refactoring the SinlgyLinkedList class template

--- Graph store/Containers/Singly Linked List/SinglyLinkedList.h
+++ Graph store/Containers/Singly Linked List/SinglyLinkedList.h
@@ -5,157 +5,149 @@
 #include <assert.h>
 #include <iterator>
 
-namespace IDragnev
+namespace IDragnev::Containers
 {
-	namespace Containers
+	template <typename T>
+	class SinglyLinkedList
 	{
-		template <typename T>
-		class SinglyLinkedList
+	private:
+		static_assert(std::is_copy_constructible_v<T>,
+			          "SinglyLinkedList<T> requires T to be copy constructible");
+
+		template <typename Item>
+		struct Node
 		{
-		private:
-			static_assert(std::is_copy_constructible_v<T>,
-						  "SinglyLinkedList<T> requires T to be copy constructible");
-
-			template <typename Item>
-			struct Node
+			template <typename T>
+			Node(T&& data, Node<Item>* next = nullptr) :
+				next{ next },
+				data{ std::forward<T>(data) }
 			{
-				template <typename T>
-				Node(T&& data, Node<Item>* next = nullptr) :
-					next{ next },
-					data{ std::forward<T>(data) }
-				{
-				}
+			}
 
-				Node<Item>* next;
-				Item data;
-			};
-
-			template <typename Item, bool isConst = false>
-			class SinglyLinkedListIterator
-			{
-			private:
-				friend class SinglyLinkedList<Item>;
-				using OwnerPtr = const SinglyLinkedList<Item>*;
-				using NodePtr = std::conditional_t<isConst, const Node<Item>*, Node<Item>*>;
-
-			public:
-				using value_type = Item;
-				using difference_type = std::ptrdiff_t;
-				using iterator_category = std::forward_iterator_tag;
-				using reference = std::conditional_t<isConst, const Item&, Item&>;
-				using pointer = std::conditional_t<isConst, const Item*, Item*>;
-
-			public:
-				SinglyLinkedListIterator(const SinglyLinkedListIterator<Item, false>& source) noexcept;
-
-				pointer operator->() const;
-				reference operator*() const;
-
-				SinglyLinkedListIterator<Item, isConst>& operator++() noexcept;
-				SinglyLinkedListIterator<Item, isConst> operator++(int) noexcept;
-
-				operator bool() const noexcept;
-				bool operator!() const noexcept;
-
-				friend bool operator==(const SinglyLinkedListIterator& lhs, const SinglyLinkedListIterator& rhs) noexcept
-				{
-					return (lhs.owner == rhs.owner) && (lhs.current == rhs.current);
-				}
-
-				friend bool operator!=(const SinglyLinkedListIterator& lhs, const SinglyLinkedListIterator& rhs) noexcept
-				{
-					return !(lhs == rhs);
-				}
-									   
-			private:
-				SinglyLinkedListIterator(NodePtr startNode, OwnerPtr owner) noexcept;
-
-			private:
-				NodePtr current;
-				OwnerPtr owner;
-			};
-
-		public:
-			using iterator = SinglyLinkedListIterator<T, false>;
-			using const_iterator = SinglyLinkedListIterator<T, true>;
-
-			SinglyLinkedList() noexcept;
-			template <typename InputIt>
-			SinglyLinkedList(InputIt first, InputIt last);
-			SinglyLinkedList(std::initializer_list<T> source);
-			SinglyLinkedList(SinglyLinkedList<T>&& source) noexcept;
-			SinglyLinkedList(const SinglyLinkedList<T>& source);
-			~SinglyLinkedList();
-
-			SinglyLinkedList<T>& operator=(SinglyLinkedList<T>&& rhs) noexcept;
-			SinglyLinkedList<T>& operator=(const SinglyLinkedList<T>& rhs);
-
-		public:
-			void insert(const T& item);
-			void insertAsHead(const T& item);
-			void insertAsTail(const T& item);
-			void insertAfter(iterator& position, const T& item);
-			void insertBefore(iterator& position, const T& item);
-
-			void removeHead();
-			void removeTail();
-			void removeAt(iterator& position);
-			void removeBefore(iterator& position);
-			void removeAfter(iterator& position);
-
-			void appendList(SinglyLinkedList<T>&& other) noexcept;
-			void appendList(const SinglyLinkedList<T>& other);
-
-			void empty() noexcept;
-			bool isEmpty() const noexcept;
-			std::size_t getCount() const noexcept;
-
-			iterator begin() noexcept;
-			iterator end() noexcept;
-			const_iterator begin() const noexcept;
-			const_iterator end() const noexcept;
-			const_iterator cbegin() const noexcept;
-			const_iterator cend() const noexcept;
-
-			void setHead(const T& item);
-			void setTail(const T& item);
-			const T& getHead() const;
-			const T& getTail() const;
-
-		private:
-			void swapContentsWithReconstructedParameter(SinglyLinkedList<T> other) noexcept;
-			void appendContentOf(SinglyLinkedList<T>&& source);
-
-			template <typename InputIt>
-			void tryToBuildChain(InputIt first, InputIt last);
-			template <typename InputIt>
-			void buildChain(InputIt first, InputIt last);
-
-			void clearCurrentChain() noexcept;
-			void nullifyMembers() noexcept;
-
-			void removeAt(Node<T>* node);
-			void insertAfter(Node<T>* node, const T& item);
-			void insertBefore(Node<T>* node, const T& item);
-			Node<T>* findNodeBefore(const Node<T>* node) const;
-
-			bool validateOwnershipOf(const iterator& it) const noexcept;
-
-		private:
-			std::size_t count;
-			Node<T>* head;
-			Node<T>* tail;
+			Node<Item>* next;
+			Item data;
 		};
 
-		template <typename T>
-		bool operator==(const SinglyLinkedList<T>& lhs, const SinglyLinkedList<T>& rhs) noexcept(noexcept(std::declval<T>() == std::declval<T>()));
-		template <typename T>
-		bool operator!=(const SinglyLinkedList<T>& lhs, const SinglyLinkedList<T>& rhs) noexcept(noexcept(lhs == rhs));
+		template <typename Item, bool isConst = false>
+		class SinglyLinkedListIterator
+		{
+		private:
+			friend class SinglyLinkedList<Item>;
+			using OwnerPtr = const SinglyLinkedList<Item>*;
+			using NodePtr = std::conditional_t<isConst, const Node<Item>*, Node<Item>*>;
 
-		template <typename T, bool isConst>
-		bool operator!=(typename const SinglyLinkedList<T>::SinglyLinkedListIterator<T, isConst>& lhs,
-						typename const SinglyLinkedList<T>::SinglyLinkedListIterator<T, isConst>& rhs) noexcept;
-	}
+		public:
+			using value_type = Item;
+			using difference_type = std::ptrdiff_t;
+			using iterator_category = std::forward_iterator_tag;
+			using reference = std::conditional_t<isConst, const Item&, Item&>;
+			using pointer = std::conditional_t<isConst, const Item*, Item*>;
+
+		public:
+			SinglyLinkedListIterator(const SinglyLinkedListIterator<Item, false>& source) noexcept;
+
+			pointer operator->() const;
+			reference operator*() const;
+
+			SinglyLinkedListIterator<Item, isConst>& operator++() noexcept;
+			SinglyLinkedListIterator<Item, isConst> operator++(int) noexcept;
+
+			operator bool() const noexcept;
+			bool operator!() const noexcept;
+
+			friend bool operator==(const SinglyLinkedListIterator& lhs, const SinglyLinkedListIterator& rhs) noexcept
+			{
+				return (lhs.owner == rhs.owner) && (lhs.current == rhs.current);
+			}
+
+			friend bool operator!=(const SinglyLinkedListIterator& lhs, const SinglyLinkedListIterator& rhs) noexcept
+			{
+				return !(lhs == rhs);
+			}
+
+		private:
+			SinglyLinkedListIterator(NodePtr startNode, OwnerPtr owner) noexcept;
+
+		private:
+			NodePtr current;
+			OwnerPtr owner;
+		};
+
+	public:
+		using iterator = SinglyLinkedListIterator<T, false>;
+		using const_iterator = SinglyLinkedListIterator<T, true>;
+
+		SinglyLinkedList() noexcept;
+		template <typename InputIt>
+		SinglyLinkedList(InputIt first, InputIt last);
+		SinglyLinkedList(std::initializer_list<T> source);
+		SinglyLinkedList(SinglyLinkedList&& source) noexcept;
+		SinglyLinkedList(const SinglyLinkedList& source);
+		~SinglyLinkedList();
+
+		SinglyLinkedList<T>& operator=(SinglyLinkedList&& rhs) noexcept;
+		SinglyLinkedList<T>& operator=(const SinglyLinkedList& rhs);
+
+	public:
+		void insert(const T& item);
+		void insertAsHead(const T& item);
+		void insertAsTail(const T& item);
+		void insertAfter(iterator& position, const T& item);
+		void insertBefore(iterator& position, const T& item);
+
+		void removeHead();
+		void removeTail();
+		void removeAt(iterator& position);
+		void removeBefore(iterator& position);
+		void removeAfter(iterator& position);
+
+		void appendList(SinglyLinkedList other) noexcept;
+
+		void empty() noexcept;
+		bool isEmpty() const noexcept;
+		std::size_t getCount() const noexcept;
+
+		iterator begin() noexcept;
+		iterator end() noexcept;
+		const_iterator begin() const noexcept;
+		const_iterator end() const noexcept;
+		const_iterator cbegin() const noexcept;
+		const_iterator cend() const noexcept;
+
+		void setHead(const T& item);
+		void setTail(const T& item);
+		const T& getHead() const;
+		const T& getTail() const;
+
+	private:
+		void swapContentsWith(SinglyLinkedList other) noexcept;
+		void appendContentOf(SinglyLinkedList&& source);
+
+		template <typename InputIt>
+		void tryToBuildChain(InputIt first, InputIt last);
+		template <typename InputIt>
+		void buildChain(InputIt first, InputIt last);
+
+		void clearCurrentChain() noexcept;
+		void nullifyMembers() noexcept;
+
+		void removeAt(Node<T>* node);
+		void insertAfter(Node<T>* node, const T& item);
+		void insertBefore(Node<T>* node, const T& item);
+		Node<T>* findNodeBefore(const Node<T>* node) const;
+
+		bool validateOwnershipOf(const iterator& it) const noexcept;
+
+	private:
+		std::size_t count;
+		Node<T>* head;
+		Node<T>* tail;
+	};
+
+	template <typename T>
+	bool operator==(const SinglyLinkedList<T>& lhs, const SinglyLinkedList<T>& rhs) noexcept(noexcept(std::declval<T>() == std::declval<T>()));
+	template <typename T>
+	bool operator!=(const SinglyLinkedList<T>& lhs, const SinglyLinkedList<T>& rhs) noexcept(noexcept(lhs == rhs));
 }
 
 #include "SinglyLinkedListImpl.hpp"
--- Graph store/Containers/Singly Linked List/SinglyLinkedListImpl.hpp
+++ Graph store/Containers/Singly Linked List/SinglyLinkedListImpl.hpp
@@ -1,442 +1,434 @@
 
-namespace IDragnev
+namespace IDragnev::Containers
 {
-	namespace Containers
+
+	template <typename T>
+	SinglyLinkedList<T>::SinglyLinkedList() noexcept :
+		count{ 0 },
+		head{ nullptr },
+		tail{ nullptr }
 	{
-		template <typename T>
-		SinglyLinkedList<T>::SinglyLinkedList() noexcept :
-			count{ 0 },
-			head{ nullptr },
-			tail{ nullptr }
+	}
+
+	template <typename T>
+	inline SinglyLinkedList<T>::SinglyLinkedList(std::initializer_list<T> source) :
+		SinglyLinkedList(source.begin(), source.end())
+	{
+	}
+
+	template <typename T>
+	inline SinglyLinkedList<T>::SinglyLinkedList(const SinglyLinkedList<T>& source) :
+		SinglyLinkedList<T>(source.cbegin(), source.cend())
+	{
+	}
+
+	template <typename T>
+	template <typename InputIt>
+	SinglyLinkedList<T>::SinglyLinkedList(InputIt first, InputIt last) :
+		SinglyLinkedList()
+	{
+		if (first != last)
 		{
+			tryToBuildChain(first, last);
+		}
+	}
+
+	template <typename T>
+	template <typename InputIt>
+	void SinglyLinkedList<T>::tryToBuildChain(InputIt first, InputIt last)
+	{
+		try
+		{
+			buildChain(first, last);
+		}
+		catch (...)
+		{
+			empty();
+			throw;
+		}
+	}
+
+	template <typename T>
+	template <typename InputIt>
+	void SinglyLinkedList<T>::buildChain(InputIt first, InputIt last)
+	{
+		assert(!head);
+		assert(first != last);
+
+		head = new Node<T>{ *first++ };
+		tail = head;
+		count = 1;
+
+		for (auto current = first; current != last; ++current)
+		{
+			tail->next = new Node<T>{ *current };
+			tail = tail->next;
+			++count;
+		}
+	}
+
+	template <typename T>
+	void SinglyLinkedList<T>::empty() noexcept
+	{
+		clearCurrentChain();
+		nullifyMembers();
+	}
+
+	template <typename T>
+	void SinglyLinkedList<T>::clearCurrentChain() noexcept
+	{
+		while (head)
+		{
+			auto oldHead = head;
+			head = head->next;
+			delete oldHead;
+		}
+	}
+
+	template <typename T>
+	inline void SinglyLinkedList<T>::nullifyMembers() noexcept
+	{
+		count = 0;
+		head = tail = nullptr;
+	}
+
+	template <typename T>
+	SinglyLinkedList<T>::SinglyLinkedList(SinglyLinkedList&& source) noexcept :
+		head{ source.head },
+		tail{ source.tail },
+		count{ source.count }
+	{
+		source.nullifyMembers();
+	}
+
+	template <typename T>
+	SinglyLinkedList<T>& SinglyLinkedList<T>::operator=(const SinglyLinkedList& rhs)
+	{
+		if (this != &rhs)
+		{
+			swapContentsWith(rhs);
 		}
 
-		template <typename T>
-		inline SinglyLinkedList<T>::SinglyLinkedList(std::initializer_list<T> source) :
-			SinglyLinkedList(source.begin(), source.end())
+		return *this;
+	}
+
+	template <typename T>
+	SinglyLinkedList<T>& SinglyLinkedList<T>::operator=(SinglyLinkedList&& rhs) noexcept
+	{
+		if (this != &rhs)
 		{
+			swapContentsWith(std::move(rhs));
 		}
 
-		template <typename T>
-		inline SinglyLinkedList<T>::SinglyLinkedList(const SinglyLinkedList<T>& source) :
-			SinglyLinkedList<T>(source.cbegin(), source.cend())
+		return *this;
+	}
+
+	template <typename T>
+	inline void SinglyLinkedList<T>::swapContentsWith(SinglyLinkedList temp) noexcept
+	{
+		std::swap(head, temp.head);
+		std::swap(tail, temp.tail);
+		std::swap(count, temp.count);
+	}
+
+	template <typename T>
+	inline SinglyLinkedList<T>::~SinglyLinkedList()
+	{
+		clearCurrentChain();
+	}
+
+	template <typename T>
+	inline void SinglyLinkedList<T>::appendList(SinglyLinkedList source) noexcept
+	{
+		if (!source.isEmpty())
 		{
-		}
-
-		template <typename T>
-		template <typename InputIt>
-		SinglyLinkedList<T>::SinglyLinkedList(InputIt first, InputIt last) :
-			SinglyLinkedList()
-		{
-			if (first != last)
-			{
-				tryToBuildChain(first, last);
-			}
-		}
-
-		template <typename T>
-		template <typename InputIt>
-		inline void SinglyLinkedList<T>::tryToBuildChain(InputIt first, InputIt last)
-		{
-			try
-			{
-				buildChain(first, last);
-			}
-			catch (...)
-			{
-				empty();
-				throw;
-			}
-		}
-
-		template <typename T>
-		template <typename InputIt>
-		void SinglyLinkedList<T>::buildChain(InputIt first, InputIt last)
-		{
-			assert(!head);
-			assert(first != last);
-
-			head = new Node<T>{ *first++ };
-			tail = head;
-			count = 1;
-
-			for (; first != last; ++first)
-			{
-				tail->next = new Node<T>{ *first };
-				tail = tail->next;
-				++count;
-			}
-		}
-
-		template <typename T>
-		void SinglyLinkedList<T>::empty() noexcept
-		{
-			clearCurrentChain();
-			nullifyMembers();
-		}
-
-		template <typename T>
-		void SinglyLinkedList<T>::clearCurrentChain() noexcept
-		{
-			while (head)
-			{
-				auto oldHead = head;
-				head = head->next;
-				delete oldHead;
-			}
-		}
-
-		template <typename T>
-		inline void SinglyLinkedList<T>::nullifyMembers() noexcept
-		{
-			count = 0;
-			head = tail = nullptr;
-		}
-
-		template <typename T>
-		SinglyLinkedList<T>::SinglyLinkedList(SinglyLinkedList<T>&& source) noexcept :
-			head{ source.head },
-			tail{ source.tail },
-			count{ source.count }
-		{
+			appendContentOf(std::move(source));
 			source.nullifyMembers();
 		}
+	}
 
-		template <typename T>
-		SinglyLinkedList<T>& SinglyLinkedList<T>::operator=(const SinglyLinkedList<T>& rhs)
+	template <typename T>
+	inline bool SinglyLinkedList<T>::isEmpty() const noexcept
+	{
+		return count == 0;
+	}
+
+	template <typename T>
+	void SinglyLinkedList<T>::appendContentOf(SinglyLinkedList&& source)
+	{
+		assert(source.head);
+		assert(source.tail);
+
+		if (isEmpty())
 		{
-			if (this != &rhs)
-			{
-				swapContentsWithReconstructedParameter(rhs);
-			}
-
-			return *this;
+			head = source.head;
+		}
+		else
+		{
+			tail->next = source.head;
 		}
 
-		template <typename T>
-		SinglyLinkedList<T>& SinglyLinkedList<T>::operator=(SinglyLinkedList<T>&& rhs) noexcept
-		{
-			if (this != &rhs)
-			{
-				swapContentsWithReconstructedParameter(std::move(rhs));
-			}
+		tail = source.tail;
+		count += source.count;
+	}
 
-			return *this;
+	template <typename T>
+	inline void SinglyLinkedList<T>::insert(const T& item)
+	{
+		insertAsTail(item);
+	}
+
+	template <typename T>
+	void SinglyLinkedList<T>::insertAsTail(const T& item)
+	{
+		auto newTail = new Node<T>{ item };
+
+		if (isEmpty())
+		{
+			head = newTail;
+		}
+		else
+		{
+			tail->next = newTail;
 		}
 
-		template <typename T>
-		inline void SinglyLinkedList<T>::swapContentsWithReconstructedParameter(SinglyLinkedList<T> temp) noexcept
+		tail = newTail;
+		++count;
+	}
+
+	template <typename T>
+	void SinglyLinkedList<T>::insertAsHead(const T& item)
+	{
+		auto newHead = new Node<T>{ item, head };
+
+		if (isEmpty())
 		{
-			std::swap(head, temp.head);
-			std::swap(tail, temp.tail);
-			std::swap(count, temp.count);
+			tail = newHead;
 		}
 
-		template <typename T>
-		inline SinglyLinkedList<T>::~SinglyLinkedList()
-		{
-			clearCurrentChain();
-		}
+		head = newHead;
+		++count;
+	}
 
-		template <typename T>
-		inline void SinglyLinkedList<T>::appendList(const SinglyLinkedList<T>& source)
-		{
-			appendList(SinglyLinkedList<T>{ source });
-		}
-
-		template <typename T>
-		void SinglyLinkedList<T>::appendList(SinglyLinkedList<T>&& source) noexcept
-		{
-			if (!source.isEmpty())
-			{
-				appendContentOf(std::move(source));
-				source.nullifyMembers();
-			}
-		}
-
-		template <typename T>
-		inline bool SinglyLinkedList<T>::isEmpty() const noexcept
-		{
-			return count == 0;
-		}
-
-		template <typename T>
-		void SinglyLinkedList<T>::appendContentOf(SinglyLinkedList<T>&& source)
-		{
-			assert(source.head);
-			assert(source.tail);
-
-			if (isEmpty())
-			{
-				head = source.head;
-			}
-			else
-			{
-				tail->next = source.head;
-			}
-
-			tail = source.tail;
-			count += source.count;
-		}
-
-		template <typename T>
-		inline void SinglyLinkedList<T>::insert(const T& item)
+	template <typename T>
+	void SinglyLinkedList<T>::insertAfter(Node<T>* node, const T& item)
+	{
+		if (!node || node == tail)
 		{
 			insertAsTail(item);
 		}
-
-		template <typename T>
-		void SinglyLinkedList<T>::insertAsTail(const T& item)
+		else
 		{
-			auto newTail = new Node<T>{ item };
-
-			if (isEmpty())
-			{
-				head = newTail;
-			}
-			else
-			{
-				tail->next = newTail;
-			}
-
-			tail = newTail;
+			assert(node->next);
+			auto newNode = new Node<T>{ item, node->next };
+			node->next = newNode;
 			++count;
 		}
+	}
 
-		template <typename T>
-		void SinglyLinkedList<T>::insertAsHead(const T& item)
+	template <typename T>
+	void SinglyLinkedList<T>::insertBefore(Node<T>* node, const T& item)
+	{
+		if (!node || node == head)
 		{
-			auto newHead = new Node<T>{ item, head };
+			insertAsHead(item);
+		}
+		else
+		{
+			auto previous = findNodeBefore(node);
+			assert(previous);
+			insertAfter(previous, item);
+		}
+	}
 
-			if (isEmpty())
-			{
-				tail = newHead;
-			}
+	template <typename T>
+	auto SinglyLinkedList<T>::findNodeBefore(const Node<T>* node) const -> Node<T>*
+	{
+		assert(node);
 
-			head = newHead;
-			++count;
+		auto current = head;
+
+		while (current && current->next != node)
+		{
+			current = current->next;
 		}
 
-		template <typename T>
-		void SinglyLinkedList<T>::insertAfter(Node<T>* node, const T& item)
+		return current;
+	}
+
+	template <typename T>
+	void SinglyLinkedList<T>::removeHead()
+	{
+		assert(!isEmpty());
+
+		auto oldHead = head;
+		head = head->next;
+
+		if (!head)
 		{
-			if (!node || node == tail)
-			{
-				insertAsTail(item);
-			}
-			else
-			{
-				assert(node->next);
-				auto newNode = new Node<T>{ item, node->next };
-				node->next = newNode;
-				++count;
-			}
+			tail = nullptr;
 		}
 
-		template <typename T>
-		void SinglyLinkedList<T>::insertBefore(Node<T>* node, const T& item)
+		delete oldHead;
+		--count;
+	}
+
+	template <typename T>
+	inline void SinglyLinkedList<T>::removeTail()
+	{
+		assert(!isEmpty());
+		removeAt(tail);
+	}
+
+	template <typename T>
+	void SinglyLinkedList<T>::removeAt(Node<T>* node)
+	{
+		if (!node)
 		{
-			if (!node || node == head)
-			{
-				insertAsHead(item);
-			}
-			else
-			{
-				auto previous = findNodeBefore(node);
-				assert(previous);
-				insertAfter(previous, item);
-			}
+			return;
 		}
-
-		template <typename T>
-		auto SinglyLinkedList<T>::findNodeBefore(const Node<T>* node) const -> Node<T>*
+		else if (node == head)
 		{
-			assert(node);
-
-			auto current = head;
-
-			while (current && current->next != node)
-			{
-				current = current->next;
-			}
-
-			return current;
+			removeHead();
 		}
-
-		template <typename T>
-		void SinglyLinkedList<T>::removeHead()
+		else
 		{
-			assert(!isEmpty());
+			auto nodeBefore = findNodeBefore(node);
+			assert(nodeBefore);
 
-			auto oldHead = head;
-			head = head->next;
-
-			if (!head)
+			if (node == tail)
 			{
-				tail = nullptr;
+				tail = nodeBefore;
 			}
 
-			delete oldHead;
+			nodeBefore->next = node->next;
+			delete node;
 			--count;
 		}
+	}
 
-		template <typename T>
-		inline void SinglyLinkedList<T>::removeTail()
-		{
-			assert(!isEmpty());
-			removeAt(tail);
-		}
+	template <typename T>
+	inline void SinglyLinkedList<T>::removeAt(iterator& it)
+	{
+		assert(validateOwnershipOf(it));
+		removeAt(it.current);
+		it.current = nullptr;
+	}
 
-		template <typename T>
-		void SinglyLinkedList<T>::removeAt(Node<T>* node)
-		{
-			if (!node)
-			{
-				return;
-			}
-			else if (node == head)
-			{
-				removeHead();
-			}
-			else
-			{
-				auto nodeBefore = findNodeBefore(node);
-				assert(nodeBefore);
+	template <typename T>
+	inline void SinglyLinkedList<T>::removeAfter(iterator& it)
+	{
+		assert(validateOwnershipOf(it));
+		removeAt(it.current->next);
+	}
 
-				if (node == tail)
-				{
-					tail = nodeBefore;
-				}
+	template <typename T>
+	inline void SinglyLinkedList<T>::removeBefore(iterator& it)
+	{
+		assert(validateOwnershipOf(it));
+		removeAt(findNodeBefore(it.current));
+	}
 
-				nodeBefore->next = node->next;
-				delete node;
-				--count;
-			}
-		}
+	template <typename T>
+	inline void SinglyLinkedList<T>::insertAfter(iterator& it, const T& item)
+	{
+		assert(validateOwnershipOf(it));
+		insertAfter(it.current, item);
+	}
 
-		template <typename T>
-		inline void SinglyLinkedList<T>::removeAt(iterator& it)
-		{
-			assert(validateOwnershipOf(it));
-			removeAt(it.current);
-			it.current = nullptr;
-		}
+	template <typename T>
+	inline void SinglyLinkedList<T>::insertBefore(iterator& it, const T& item)
+	{
+		assert(validateOwnershipOf(it));
+		insertBefore(it.current, item);
+	}
 
-		template <typename T>
-		inline void SinglyLinkedList<T>::removeAfter(iterator& it)
-		{
-			assert(validateOwnershipOf(it));
-			removeAt(it.current->next);
-		}
+	template <typename T>
+	inline bool SinglyLinkedList<T>::validateOwnershipOf(const iterator& it) const noexcept
+	{
+		return it.owner == this;
+	}
 
-		template <typename T>
-		inline void SinglyLinkedList<T>::removeBefore(iterator& it)
-		{
-			assert(validateOwnershipOf(it));
-			removeAt(findNodeBefore(it.current));
-		}
+	template <typename T>
+	inline const T& SinglyLinkedList<T>::getHead() const
+	{
+		assert(!isEmpty());
+		return head->data;
+	}
 
-		template <typename T>
-		inline void SinglyLinkedList<T>::insertAfter(iterator& it, const T& item)
-		{
-			assert(validateOwnershipOf(it));
-			insertAfter(it.current, item);
-		}
+	template <typename T>
+	inline const T& SinglyLinkedList<T>::getTail() const
+	{
+		assert(!isEmpty());
+		return tail->data;
+	}
 
-		template <typename T>
-		inline void SinglyLinkedList<T>::insertBefore(iterator& it, const T& item)
-		{
-			assert(validateOwnershipOf(it));
-			insertBefore(it.current, item);
-		}
+	template <typename T>
+	inline void SinglyLinkedList<T>::setHead(const T& data)
+	{
+		assert(!isEmpty());
+		head->data = data;
+	}
 
-		template <typename T>
-		inline bool SinglyLinkedList<T>::validateOwnershipOf(const iterator& it) const noexcept
-		{
-			return it.owner == this;
-		}
+	template <typename T>
+	inline void SinglyLinkedList<T>::setTail(const T& data)
+	{
+		assert(!isEmpty());
+		tail->data = data;
+	}
 
-		template <typename T>
-		inline const T& SinglyLinkedList<T>::getHead() const
-		{
-			assert(!isEmpty());
-			return head->data;
-		}
+	template <typename T>
+	inline std::size_t SinglyLinkedList<T>::getCount() const noexcept
+	{
+		return count;
+	}
 
-		template <typename T>
-		inline const T& SinglyLinkedList<T>::getTail() const
-		{
-			assert(!isEmpty());
-			return tail->data;
-		}
+	template <typename T>
+	inline auto SinglyLinkedList<T>::cbegin() const noexcept -> const_iterator
+	{
+		return const_iterator{ head, this };
+	}
 
-		template <typename T>
-		inline void SinglyLinkedList<T>::setHead(const T& data)
-		{
-			assert(!isEmpty());
-			head->data = data;
-		}
+	template <typename T>
+	inline auto SinglyLinkedList<T>::cend() const noexcept -> const_iterator
+	{
+		return const_iterator{ nullptr, this };
+	}
 
-		template <typename T>
-		inline void SinglyLinkedList<T>::setTail(const T& data)
-		{
-			assert(!isEmpty());
-			tail->data = data;
-		}
+	template <typename T>
+	inline auto SinglyLinkedList<T>::begin() noexcept -> iterator
+	{
+		return iterator{ head, this };
+	}
 
-		template <typename T>
-		inline std::size_t SinglyLinkedList<T>::getCount() const noexcept
-		{
-			return count;
-		}
+	template <typename T>
+	inline auto SinglyLinkedList<T>::end() noexcept -> iterator
+	{
+		return iterator{ nullptr, this };
+	}
 
-		template <typename T>
-		inline auto SinglyLinkedList<T>::cbegin() const noexcept -> const_iterator
-		{
-			return const_iterator{ head, this };
-		}
+	template <typename T>
+	inline auto SinglyLinkedList<T>::begin() const noexcept -> const_iterator
+	{
+		return cbegin();
+	}
 
-		template <typename T>
-		inline auto SinglyLinkedList<T>::cend() const noexcept -> const_iterator
-		{
-			return const_iterator{ nullptr, this };
-		}
+	template <typename T>
+	inline auto SinglyLinkedList<T>::end() const noexcept -> const_iterator
+	{
+		return cend();
+	}
 
-		template <typename T>
-		inline auto SinglyLinkedList<T>::begin() noexcept -> iterator
-		{
-			return iterator{ head, this };
-		}
+	template <typename T>
+	inline bool operator==(const SinglyLinkedList<T>& lhs, const SinglyLinkedList<T>& rhs) noexcept(noexcept(std::declval<T>() == std::declval<T>()))
+	{
+		return std::equal(lhs.cbegin(), lhs.cend(),
+			              rhs.cbegin(), rhs.cend());
+	}
 
-		template <typename T>
-		inline auto SinglyLinkedList<T>::end() noexcept -> iterator
-		{
-			return iterator{ nullptr, this };
-		}
-
-		template <typename T>
-		inline auto SinglyLinkedList<T>::begin() const noexcept -> const_iterator
-		{
-			return cbegin();
-		}
-
-		template <typename T>
-		inline auto SinglyLinkedList<T>::end() const noexcept -> const_iterator
-		{
-			return cend();
-		}
-
-		template <typename T>
-		inline bool operator==(const SinglyLinkedList<T>& lhs, const SinglyLinkedList<T>& rhs) noexcept(noexcept(std::declval<T>() == std::declval<T>()))
-		{
-			return std::equal(lhs.cbegin(), lhs.cend(),
-							  rhs.cbegin(), rhs.cend());
-		}
-
-		template <typename T>
-		inline bool operator!=(const SinglyLinkedList<T>& lhs, const SinglyLinkedList<T>& rhs) noexcept(noexcept(lhs == rhs))
-		{
-			return !(lhs == rhs);
-		}
+	template <typename T>
+	inline bool operator!=(const SinglyLinkedList<T>& lhs, const SinglyLinkedList<T>& rhs) noexcept(noexcept(lhs == rhs))
+	{
+		return !(lhs == rhs);
 	}
 }
