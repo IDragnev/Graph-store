4adbf3f157fcf30c3004711e98ad2867031a7841
IDragnev <iddragnev@gmail.com>
2019-04-24 20:18:49 +0300

    Move the MissingArgument class in the Command header
    Since it must be thrown in RemoveGraphCommand's implementation file,
    its definition must be visible.

--- Graph store/Application/Commands/Base/Command.cpp
+++ Graph store/Application/Commands/Base/Command.cpp
@@ -1,88 +1,79 @@
 #include "Command.h"
-#include "..\..\..\String\String.h"
-#include "..\..\..\GraphStore\GraphStore.h"
-#include "..\..\..\Graph\Base Graph\Graph.h"
-#include "..\..\..\General Exceptions\Exception.h"
-#include "..\..\..\..\Third party\args\args.hxx"
+#include "String\String.h"
+#include "GraphStore\GraphStore.h"
+#include "Graph\Base Graph\Graph.h"
+#include "Exceptions\Exceptions.h"
+#include "Third party\args\args.hxx"
 
-namespace IDragnev
+namespace IDragnev::GraphStore
 {
-	namespace GraphStore
+	Graph* Command::usedGraph = nullptr;
+	GraphStore* Command::graphStore = nullptr;
+
+	void Command::execute(args::Subparser& parser)
 	{
-		class MissingArgument : public Exception
-		{
-		public:
-			MissingArgument(const std::string& name) :
-				Exception{ "Missing argument: [" + name + "]" } {}
-		};
+		parseArguments(parser);
+		execute();
+	}
 
-		Graph* Command::usedGraph = nullptr;
-		GraphStore* Command::graphStore = nullptr;
+	void Command::setIfMatched(String& str, StringPositional& argument)
+	{
+		using args::get;
 
-		void Command::execute(args::Subparser& parser)
+		if (argument.Matched())
 		{
-			parseArguments(parser);
-			execute();
+			str = std::move(get(argument));
 		}
-
-		void Command::setIfMatched(String& str, StringPositional& argument)
+		else
 		{
-			if (argument.Matched())
-			{
-				using args::get;
-				str = std::move(get(argument));
-			}
-			else
-			{
-				throw MissingArgument{ argument.Name() };
-			}
+			throw MissingArgument{ argument.Name() };
 		}
+	}
 
-		void Command::setManagedStore(GraphStore& store)
+	void Command::setManagedStore(GraphStore& store)
+	{
+		graphStore = &store;
+		usedGraph = nullptr;
+	}
+
+	void Command::useGraph(const String& ID)
+	{
+		usedGraph = &getGraph(ID);
+	}
+
+	Graph& Command::getGraph(const String& ID)
+	{
+		assert(graphStore);
+		return graphStore->getGraph(ID);
+	}
+
+	Graph& Command::getUsedGraph()
+	{
+		if (usedGraph)
 		{
-			graphStore = &store;
+			return *usedGraph;
+		}
+		else
+		{
+			throw Exception{ "No graph is currently being used" };
+		}
+	}
+
+	void Command::insertGraph(std::unique_ptr<Graph> ptr)
+	{
+		assert(graphStore);
+		graphStore->insertGraph(std::move(ptr));
+	}
+
+	void Command::removeGraph(const String& ID)
+	{
+		assert(graphStore);
+
+		if (usedGraph && usedGraph->getID() == ID)
+		{
 			usedGraph = nullptr;
 		}
 
-		void Command::useGraph(const String& ID)
-		{
-			usedGraph = &getGraph(ID);
-		}
-
-		Graph& Command::getGraph(const String& ID)
-		{
-			assert(graphStore);
-			return graphStore->getGraph(ID);
-		}
-
-		Graph& Command::getUsedGraph()
-		{
-			if (usedGraph)
-			{
-				return *usedGraph;
-			}
-			else
-			{
-				throw Exception{ "No graph is currently being used" };
-			}
-		}
-
-		void Command::insertGraph(std::unique_ptr<Graph> ptr)
-		{
-			assert(graphStore);
-			graphStore->insertGraph(std::move(ptr));
-		}
-
-		void Command::removeGraph(const String& ID)
-		{
-			assert(graphStore);
-
-			if (usedGraph && usedGraph->getID() == ID)
-			{
-				usedGraph = nullptr;
-			}
-
-			graphStore->removeGraph(ID);
-		}
+		graphStore->removeGraph(ID);
 	}
 }
--- Graph store/Application/Commands/Base/Command.h
+++ Graph store/Application/Commands/Base/Command.h
@@ -2,6 +2,7 @@
 #define __COMMAND_BASE_H_INCLUDED__
 
 #include <memory>
+#include "Exceptions\Exceptions.h"
 
 namespace args
 {
@@ -16,59 +17,66 @@ namespace args
 namespace IDragnev
 {
 	class String;
+}
 
-	namespace GraphStore
+namespace IDragnev::GraphStore
+{
+	class Graph;
+	class GraphStore;
+
+	class Command
 	{
-		class Graph;
-		class GraphStore;
-
-		class Command
+	protected:
+		class MissingArgument : public Exception
 		{
-		protected:
-			struct StringReader
-			{
-				template <typename Name, typename Source, typename Destination>
-				void operator()(const Name&, const Source& value, Destination& dest) const
-				{
-					dest = value.c_str();
-				}
-			};
-
-			template <typename T>
-			using Positional = args::Positional<T, args::ValueReader>;
-			using StringPositional = args::Positional<String, StringReader>;
-
 		public:
-			Command() = default;
-			virtual ~Command() = default;
-
-			Command(const Command&) = delete;
-			Command& operator=(const Command&) = delete;
-
-			void execute(args::Subparser& parser);
-			virtual const char* getName() const noexcept = 0;
-			virtual const char* getDescription() const noexcept = 0;
-
-			static void setManagedStore(GraphStore& store);
-
-		protected:
-			static void setIfMatched(String& str, StringPositional& argument);
-
-			static Graph& getUsedGraph();
-			static Graph& getGraph(const String& ID);
-			static void useGraph(const String& ID);
-			static void removeGraph(const String& ID);
-			static void insertGraph(std::unique_ptr<Graph> graphPtr);
-			
-		private:
-			virtual void parseArguments(args::Subparser& parser) = 0;
-			virtual void execute() = 0;
-
-		private:
-			static Graph* usedGraph;
-			static GraphStore* graphStore;
+			MissingArgument(const std::string& name) :
+				Exception{ "Missing argument: [" + name + "]" } {}
 		};
-	}
+
+		struct StringReader
+		{
+			template <typename Name, typename Source, typename Destination>
+			void operator()(const Name&, const Source& value, Destination& dest) const
+			{
+				dest = value.c_str();
+			}
+		};
+
+		template <typename T>
+		using Positional = args::Positional<T, args::ValueReader>;
+		using StringPositional = args::Positional<String, StringReader>;
+
+	public:
+		Command() = default;
+		virtual ~Command() = default;
+
+		Command(const Command&) = delete;
+		Command& operator=(const Command&) = delete;
+
+		void execute(args::Subparser& parser);
+		virtual const char* getName() const noexcept = 0;
+		virtual const char* getDescription() const noexcept = 0;
+
+		static void setManagedStore(GraphStore& store);
+
+	protected:
+		static void setIfMatched(String& str, StringPositional& argument);
+
+		static Graph& getUsedGraph();
+		static Graph& getGraph(const String& ID);
+		static void useGraph(const String& ID);
+		static void removeGraph(const String& ID);
+		static void insertGraph(std::unique_ptr<Graph> graphPtr);
+
+	private:
+		virtual void parseArguments(args::Subparser& parser) = 0;
+		virtual void execute() = 0;
+
+	private:
+		static Graph* usedGraph;
+		static GraphStore* graphStore;
+	};
 }
 
 #endif //__COMMAND_BASE_H_INCLUDED__
