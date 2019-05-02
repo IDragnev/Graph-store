0a456de8decdc44a0ed6d06cb22a00df5d445189
IDragnev <iddragnev@gmail.com>
2019-05-02 22:48:47 +0300

    Make several traits SFINAE-friendly

--- Graph store/Traits/Traits.h
+++ Graph store/Traits/Traits.h
@@ -15,23 +15,47 @@ namespace IDragnev::Traits
 		using IteratorReference = typename std::iterator_traits<Iterator>::reference;
 	}
 
+	template <typename T, typename = std::void_t<>>
+	struct IsIterator : std::false_type { };
+
+	template <typename T>
+	struct IsIterator<T, std::void_t<Detail::IteratorCategory<T>>> : std::true_type { };
+
+	template <typename T>
+	inline constexpr bool isIterator = IsIterator<T>::value;
+
 	template <typename T>
 	struct IsConstReference : std::is_const<std::remove_reference_t<T>> { };
 	
 	template <typename T>
 	inline constexpr bool isConstReference = IsConstReference<T>::value;
 
-	template <typename Iterator>
+	template <typename Iterator, bool = isIterator<Iterator>>
 	struct IsConstIterator : IsConstReference<Detail::IteratorReference<Iterator>> { };
+
+	template <typename Iterator>
+	struct IsConstIterator<Iterator, false> : std::false_type { };
 
 	template <typename Iterator>
 	inline constexpr bool isConstIterator = IsConstIterator<Iterator>::value;
 
-	template <typename Iterator>
+	template <typename Iterator, bool = isIterator<Iterator>>
 	struct IsForwardIterator : std::is_convertible<Detail::IteratorCategory<Iterator>, std::forward_iterator_tag> { };
 
 	template <typename Iterator>
+	struct IsForwardIterator<Iterator, false> : std::false_type { };
+
+	template <typename Iterator>
 	inline constexpr bool isForwardIterator = IsForwardIterator<Iterator>::value;
+
+	template <typename Iterator, bool = isIterator<Iterator>>
+	struct IsInputIterator : std::is_convertible<Detail::IteratorCategory<Iterator>, std::input_iterator_tag> { };
+
+	template <typename Iterator>
+	struct IsInputIterator<Iterator, false> : std::false_type { };
+
+	template <typename Iterator>
+	inline constexpr bool isInputIterator = IsInputIterator<Iterator>::value;
 
 	template <template <typename...> typename Predicate, typename... Ts>
 	struct AllOf : std::bool_constant<(Predicate<Ts>::value && ...)> { };
